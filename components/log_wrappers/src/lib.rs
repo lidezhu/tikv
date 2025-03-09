27d18e9e4d83a0b44f6a09fb271d9019dc0e5912
lidezhu <lidezhu@pingcap.com>
2025-03-09 17:28:50 +0800

    fix build

--- components/log_wrappers/src/lib.rs
+++ components/log_wrappers/src/lib.rs
@@ -14,7 +14,6 @@ use std::{
     sync::atomic::{AtomicBool, Ordering},
 };
 
-use protobuf::atomic_flags::set_redact_level as proto_set_redact_bytes;
 
 pub use crate::hex::*;
 
@@ -94,7 +93,6 @@ static REDACT_INFO_LOG: AtomicBool = AtomicBool::new(false);
 /// Set whether we should avoid user data to slog.
 pub fn set_redact_info_log(v: bool) {
     REDACT_INFO_LOG.store(v, Ordering::Relaxed);
-    proto_set_redact_bytes(v);
 }
 
 pub struct Value<'a>(pub &'a [u8]);
